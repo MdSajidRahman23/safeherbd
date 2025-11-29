c8ddf9a73080beebfc3a37741f841677dfeaf9b3
Zahinkhan-svg <khan22205101473@diu.edu.bd>
2025-11-29 12:22:24 +0600

    saving my work before pull

---
+++ database/migrations/2025_11_21_170653_create_safe_routes_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('safe_routes', function (Blueprint $table) {
+            $table->id();
+            $table->string('route_name'); 
+            
+            $table->json('coordinates');  
+            $table->integer('total_score')->default(0); 
+            
+            $table->foreignId('created_by')->constrained('users')->onDelete('cascade'); 
+            
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('safe_routes');
+    }
+};
