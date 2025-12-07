ed9153091c6177a3b7aed61b4f3e01586fc7040f
marieauwu <mariea2205101527@diu.edu.bd>
2025-12-07 08:13:11 +0600

    few work

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ForumController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -19,10 +20,21 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware(['auth', 'is_woman'])->group(function () {
+    // women-only routes
+});
+
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+// Women-only forum routes
+Route::middleware(['auth', 'is_woman'])->group(function () {
+    Route::get('/forum', [ForumController::class, 'index']);
+    Route::post('/forum/post', [ForumController::class, 'store']);
+    Route::post('/forum/{post}/reply', [ForumController::class, 'reply']);
 });
 
 require __DIR__.'/auth.php';
