f8f3dd59f60e320b70f8afafc830cbfad20642ec
MdSajidRahman23 <rahman22205101027@diu.edu.bd>
2025-12-11 01:05:22 +0600

    sos-auth: create Admin SosHistoryController

---
+++ app/Http/Controllers/Admin/SosHistoryController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\SosAlert;
+use Illuminate\Http\Request;
+
+class SosHistoryController extends Controller
+{
+    /**
+     * Display a listing of all SOS alerts for admin
+     */
+    public function index()
+    {
+        $alerts = SosAlert::with('user')
+            ->orderBy('created_at', 'desc')
+            ->paginate(20);
+
+        return view('admin.sos-history', compact('alerts'));
+    }
+}
