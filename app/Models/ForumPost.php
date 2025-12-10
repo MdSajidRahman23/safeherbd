ba040d715510dc8fa42a14bdcaff279b018ea421
MdSajidRahman23 <rahman22205101027@diu.edu.bd>
2025-12-11 01:49:18 +0600

    sos-auth: fix ForumPost model (resolve merge conflicts)

--- app/Models/ForumPost.php
+++ app/Models/ForumPost.php
@@ -6,26 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class ForumPost extends Model
 {
-<<<<<<< HEAD
-protected $fillable = ['title','body','user_id'];
-
-
-public function user(){
-return $this->belongsTo(User::class);
-}
-
-
-public function replies(){
-return $this->hasMany(ForumReply::class, 'post_id');
-}
-
-
-public function forumreports(){
-return $this->hasMany(ForumReport::class, 'post_id');
-}
-}
-=======
     protected $fillable = ['title', 'body', 'user_id'];
+
     public function user()
     {
         return $this->belongsTo(User::class);
@@ -35,6 +17,9 @@ return $this->hasMany(ForumReport::class, 'post_id');
     {
         return $this->hasMany(ForumReply::class, 'post_id');
     }
-}
 
->>>>>>> b0cb2efddad894b83ece0451a04ec967047a9524
+    public function forumreports()
+    {
+        return $this->hasMany(ForumReport::class, 'post_id');
+    }
+}
